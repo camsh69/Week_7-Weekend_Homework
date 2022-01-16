e77bc872492cc96824abe0032aef881631a4ecb6
Campbell Forsyth <camsh69@gmail.com>
2022-01-16 12:45:27 +0000

    implemented google chart

--- src/containers/HeadlineContainer.js
+++ src/containers/HeadlineContainer.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import HeadlineList from '../components/HeadlineList'
 import ReadLaterList from '../components/ReadLaterList'
 import TopicSearch from '../components/TopicSearch'
-import icon from '../img/guardian-icon.jpeg'
+import { Chart } from "react-google-charts";
 import './HeadlineContainer.css'
 
 
@@ -43,35 +43,90 @@ const HeadlineContainer = () => {
         setReadLater(updatedReadLater);
     }
 
+    const parseData = () => {
+        const sections = headlines.map(section => section.sectionName);
+
+        const uniqueItemsArray = [...sections].sort()
+        .filter((item, position, array) => !position || item != array[position -1]);
+        
+        const count = (array, value) => array.filter(item => (item === value)).length;
+
+        const countArray = [];
+        for (let i = 0; i < uniqueItemsArray.length; i++){
+            let num = count(sections, uniqueItemsArray[i]);
+            countArray.push(num);
+        }
+
+        const combinedArray = [];
+        for (let i = 0; i < uniqueItemsArray.length; i++) {
+                combinedArray.push(Array(uniqueItemsArray[i], countArray[i]))
+        }
+
+        combinedArray.unshift(["Section", "Number of articles"]);
+
+        return combinedArray;
+    }
+
+    
+    const data = parseData();
+
+    
+    // [
+    //     ["Subject", "Number of articles"],
+    //     ["Politics", 4],
+    //     ["World News", 2],
+    //     ["UK News", 3],
+    //     ["Environment", 1],
+    //   ];
+
+      const options = {
+        title: "Headline by News Section",
+        pieHole: 0,
+        is3D: false
+      };
+
     return (
+        <>
         <section className='main-container'>
+            <div className='app-container'>
+                <div className='header'>
+                </div>
 
-            <div className='header'>
-                <img src={icon} alt='Guardian logo'/>
+                <h1>Guardian Headlines</h1>
+
+                <div className='search'>
+                    <TopicSearch onTopicSearchSubmit={newTopic => getHeadlines(newTopic)}/>
+                </div>
+
+                <hr/>
+
+                <h2>Top 10 Search Results for <span className='capitalize'>{headlineTopic}:</span></h2>
+
+                <div className = 'results'>
+                    <HeadlineList headlines={headlines} onHeadlineClick={url => window.open(url)} headlineAdd={headline => addToReadLater(headline)}/>
+                </div>
+
+                <h2>Read Later:</h2>
+
+                <div className = 'results'>
+                    <ReadLaterList readLater={readLater} onHeadlineClick={url => window.open(url)}  headlineRemove={headline => removeFromReadLAter(headline)}/>
+                </div>
             </div>
 
-            <h1>Guardian Headlines</h1>
+        
 
-            <div className='search'>
-                <TopicSearch onTopicSearchSubmit={newTopic => getHeadlines(newTopic)}/>
-            </div>
-
-            <hr/>
-
-            <h2>Top 10 Search Results for <span className='capitalize'>{headlineTopic}:</span></h2>
-
-            <div className = 'results'>
-                <HeadlineList headlines={headlines} onHeadlineClick={url => window.open(url)} headlineAdd={headline => addToReadLater(headline)}/>
-            </div>
-
-            <h2>Read Later:</h2>
-
-            <div className = 'results'>
-                <ReadLaterList readLater={readLater} onHeadlineClick={url => window.open(url)}  headlineRemove={headline => removeFromReadLAter(headline)}/>
-            </div>
-
+        <div className='chart'>
+                <Chart
+            chartType="PieChart"
+            data={data}
+            options={options}
+            width={"100%"}
+            height={"400px"}
+            />
+        </div>
         </section>
+        </>
     )
 }
 
-export default HeadlineContainer
+export default HeadlineContainer;
